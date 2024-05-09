b0db8b9726ecdb7a1a56ddf4fd0400a747a135d9
kirill <kirilloz21rus@gmail.com>
2024-05-09 16:58:37 +0300

    added a news window for admin

---
+++ Coffee/Controllers/AdminController.cs
@@ -0,0 +1,36 @@
+﻿using Coffee.Models.Entity;
+using Coffee.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+
+namespace Coffee.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    public class AdminController : Controller
+    {
+        private NewsRepository _newsRepository;
+
+        public AdminController(NewsRepository newsRepository)
+        {
+            _newsRepository = newsRepository;
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Users()
+        {
+            var ListUsers = new List<string>();
+
+            return View(ListUsers);
+        }
+
+        public async Task<ActionResult> News()
+        {
+            var listNews = await _newsRepository.GetNewsAsync();
+
+            return View(listNews);
+        }
+    }
+}
---
+++ Coffee/Data/ApplicationDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Coffee.Models;
+using Coffee.Models.Entity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Coffee.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<News> News => Set<News>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder
+                .Entity<User>()
+                .Property(e => e.Created)
+                .HasDefaultValueSql("now()");
+
+            modelBuilder
+                .Entity<News>()
+                .Property(e => e.Date)
+                .HasDefaultValueSql("now()");
+
+            modelBuilder
+                .Entity<News>()
+                .Property(e => e.CreateDate)
+                .HasDefaultValueSql("now()");
+
+            modelBuilder
+                .Entity<News>()
+                .Property(e => e.IsActive)
+                .HasDefaultValue(true);
+        }
+    }
+}
