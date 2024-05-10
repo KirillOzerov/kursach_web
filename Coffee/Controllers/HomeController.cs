5cc26631752b17939074f125805a3d99898eb7b8
kirill <kirilloz21rus@gmail.com>
2024-05-10 20:20:14 +0300

    add left bar button News

--- Coffee/Controllers/HomeController.cs
+++ Coffee/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace Coffee.Controllers
             return View();
         }
 
+        public IActionResult News()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
