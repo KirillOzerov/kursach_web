d1e5256c27999605a37cec9925f1d06d7f03a02a
kirill <kirilloz21rus@gmail.com>
2024-05-11 17:29:02 +0300

    add News adder in Admin/CreateNews.cshtml

--- Coffee/Controllers/AdminController.cs
+++ Coffee/Controllers/AdminController.cs
@@ -3,6 +3,7 @@ using Coffee.Repositories;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Identity.Client;
+using System.Security.Claims;
 
 namespace Coffee.Controllers
 {
@@ -33,9 +34,26 @@ namespace Coffee.Controllers
             return View(listNews);
         }
 
+        [HttpGet]
         public async Task<ActionResult> CreateNews()
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateNews(News news)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (!string.IsNullOrEmpty(userId))
+            {
+                news.AuthorId = userId;
+
+                news.Date = DateTime.SpecifyKind(news.Date, DateTimeKind.Utc);
+
+                var result = await _newsRepository.CreateNewsAsync(news);
+            }
+            return Redirect("/Admin/News");
+        }
     }
-}
+} 
